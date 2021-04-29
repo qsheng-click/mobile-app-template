b4ee81970e3083d1c87443119e664ca554097bc1
Quan Sheng <qsheng@clicktherapeutics.com>
2021-04-29 13:56:32 -0400

    Fix post init script

--- template.config.js
+++ template.config.js
@@ -10,5 +10,5 @@ module.exports = {
     templateDir: './ClickApp',
   
     // Path to script, which will be executed after initialization process, but before installing all the dependencies specified in the template. This script runs as a shell script but you can change that (e.g. to Node) by using a shebang (see example custom template).
-    postInitScript: './postInit.js',
+    postInitScript: './post-init.js',
   };
